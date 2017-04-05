46350701c3e9f0003dc3660f498144b5379f4fd8
sanae <sanae@stech.ac.th>
2017-04-05 10:42:39 +0700

    Get event click

--- app/src/main/java/stech/sukprung/sanae/mystech/MainActivity.java
+++ app/src/main/java/stech/sukprung/sanae/mystech/MainActivity.java
@@ -2,11 +2,13 @@ package stech.sukprung.sanae.mystech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit [ตัวแปร]
     private EditText userEditText, passEditText;
@@ -20,16 +22,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         ///Initial View  [ผูกตัวแปรกับ View ที่ Activity]
         initialView();
 
-
-
-
+        // create Controller
+        controller();
 
 
     }   // Main Method
+
+
+    // สร้าง  Method สำหรับควบคุม
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
+
+    }
 
 
     private void initialView() {    // void ไม่มีคืนค่ากลับ
@@ -39,4 +47,25 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.btnLogin);
 
     }
+
+    @Override
+    public void onClick(View v) {
+
+        String tag = "SriwattanaV1";
+
+        // For Text view
+        if (v == textView) {
+            Log.d(tag, "You Click TextView 'New Register'");
+        }
+
+        // For Button
+        if (v== button) {
+            Log.d(tag, "You Click Button 'Login'");
+        }
+
+
+    }   // Method On Click
+
+
+
 }   // end Main Class
